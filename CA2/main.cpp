8d776f7c24bf90d355e5d3c14ad27ac02e1cbb28
zjleee <zjlee8@gmail.com>
2023-12-14 16:15:26 +0000

    Add input and display in SFML

--- CA2/main.cpp
+++ CA2/main.cpp
@@ -2,16 +2,17 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/Font.hpp>
+#include <sstream>
 
 using namespace std;
 
 // Function prototypes for your functionalities
-void countItems(TreeNode* root, string folderName, TreeNode* folderNode);  // Define this function
-void determineMemoryUsage(TreeNode* root, string folderName, TreeNode* folderNode);  // Define this function
-void pruneEmptyFolders(TreeNode* root);  // Define this function
-void findFolder(TreeNode* root, string folderN);  // Define this function
-void displayFolderContents(TreeNode* root, string folderName, TreeNode* folderNode);  // Define this function
-void onButtonClick();
+void countItems(TreeNode* root, sf::RenderWindow& window, sf::Font& font);  // Define this function
+void determineMemoryUsage(TreeNode* root, sf::RenderWindow& window, sf::Font& font);  // Define this function
+void pruneEmptyFolders(TreeNode* root, sf::RenderWindow& window, sf::Font& font);  // Define this function
+void findFolder(TreeNode* root, sf::RenderWindow& window, sf::Font& font);  // Define this function
+void displayFolderContents(TreeNode* root, sf::RenderWindow& window, sf::Font& font);  // Define this function
+void printTreeToStream(TreeNode* node, std::stringstream& stream, int depth, bool includeSize);
 
 void printTree(TreeNode* node, int depth = 0, bool includeSize = false) {
     if (!node) return;
@@ -98,22 +99,23 @@ int main() {
                     for (int i = 0; i < numButtons; ++i) {
                         if (buttons[i].getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
                             switch (i) {
+                            case 0:
+                                countItems(root, window, font);
+                                break;
                             case 1:
-                                countItems(root, folderName, folderNode);
+                                determineMemoryUsage(root, window, font);
                                 break;
                             case 2:
-                                determineMemoryUsage(root, folderName, folderNode);
+                                pruneEmptyFolders(root, window, font);
                                 break;
                             case 3:
-                                pruneEmptyFolders(root);
+                                findFolder(root, window, font);
                                 break;
                             case 4:
-                                findFolder(root, folderName);
+                                displayFolderContents(root, window, font);
                                 break;
-                            case 5:
-                                displayFolderContents(root, folderName, folderNode);
-                            case 6: 
-                                onButtonClick();
+                            case 5: 
+                                window.close();
                                 break;
                             }
                         }
@@ -134,62 +136,346 @@ int main() {
     return 0;
 }
 
-/
-void countItems(TreeNode* root, string folderName, TreeNode* folderNode) {
+void countItems(TreeNode* root, sf::RenderWindow& window, sf::Font& font) {
+    std::string input;
+    sf::Text inputText("", font, 20);
+    inputText.setPosition(50, 50);
+    inputText.setFillColor(sf::Color::White);
+
+    sf::Text prompt("Enter folder name: ", font, 20);
+    prompt.setPosition(50, 20);
+    prompt.setFillColor(sf::Color::White);
+
+    sf::Text outputText("", font, 20);
+    outputText.setPosition(50, 100);
+    outputText.setFillColor(sf::Color::Yellow);
+
+    bool done = false;
+    while (window.isOpen() && !done) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+            else if (event.type == sf::Event::TextEntered) {
+                if (event.text.unicode == '\b' && !input.empty()) {  // Handle backspace
+                    input.pop_back();
+                }
+                else if (event.text.unicode < 128 && event.text.unicode != '\r' && event.text.unicode != '\n') {
+                    input += static_cast<char>(event.text.unicode);
+                }
+            }
+            else if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Enter) {
+                    done = true;  // User pressed Enter, finish input
+                }
+            }
+        }
+
+        inputText.setString(input);
+
+        // Redraw the window
+        window.clear();
+        window.draw(prompt);
+        window.draw(inputText);
+        window.display();
+    }
+
+    TreeNode* folderNode = root->findNode(input);
+    if (folderNode) {
+        outputText.setString("Number of items in " + input + ": " + std::to_string(folderNode->countItems()));
+    }
+    else {
+        outputText.setString("Folder not found.");
+    }
+
+    window.clear();
+    window.draw(prompt);
+    window.draw(inputText);
+    window.draw(outputText);
+    window.display();
+
+    // Wait for a few seconds or until user closes the window
+    sf::Clock clock;
+    while (window.isOpen() && clock.getElapsedTime().asSeconds() < 4 ) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+            else if (event.type == sf::Event::MouseButtonPressed) {
+                // If any mouse button is pressed, exit the loop
+                return;
+            }
+        }
+    }
+}
+
+void determineMemoryUsage(TreeNode* root, sf::RenderWindow& window, sf::Font& font) {
+    std::string input;
+    sf::Text inputText("", font, 20);
+    inputText.setPosition(50, 50);
+    inputText.setFillColor(sf::Color::White);
+
+    sf::Text prompt("Enter folder name: ", font, 20);
+    prompt.setPosition(50, 20);
+    prompt.setFillColor(sf::Color::White);
+
+    sf::Text outputText("", font, 20);
+    outputText.setPosition(50, 100);
+    outputText.setFillColor(sf::Color::Yellow);
+
+    bool done = false;
+    while (window.isOpen() && !done) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+            else if (event.type == sf::Event::TextEntered) {
+                if (event.text.unicode == '\b' && !input.empty()) {
+                    input.pop_back();
+                }
+                else if (event.text.unicode < 128 && event.text.unicode != '\r' && event.text.unicode != '\n') {
+                    input += static_cast<char>(event.text.unicode);
+                }
+            }
+            else if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Enter) {
+                    done = true;
+                }
+            }
+        }
+
+        inputText.setString(input);
+
+        window.clear();
+        window.draw(prompt);
+        window.draw(inputText);
+        window.display();
+    }
+
+    TreeNode* folderNode = root->findNode(input);
+    outputText.setPosition(50, 100);
+    outputText.setFillColor(sf::Color::Yellow);
+    if (folderNode) {
+        outputText.setString("Memory used by " + input + ": " + std::to_string(folderNode->memoryUsed()) + " bytes");
+    }
+    else {
+        outputText.setString("Folder not found.");
+    }
+
+    window.clear();
+    window.draw(prompt);
+    window.draw(inputText);
+    window.draw(outputText);
+    window.display();
+
+    sf::Clock clock;
+    while (window.isOpen() && clock.getElapsedTime().asSeconds() < 4) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+            else if (event.type == sf::Event::MouseButtonPressed) {
+                return;
+            }
+        }
+    }
+
+}
+
+void pruneEmptyFolders(TreeNode* root, sf::RenderWindow& window, sf::Font& font) {
+    int prunedCount = root->pruneEmptyFolders();
+
+    sf::Text outputText("Number of empty folders removed: " + std::to_string(prunedCount), font, 20);
+    outputText.setPosition(50, 50);
+    outputText.setFillColor(sf::Color::Yellow);
+
+    window.clear();
+    window.draw(outputText);
+    window.display();
+
+    sf::Clock clock;
+    while (window.isOpen() && clock.getElapsedTime().asSeconds() < 4) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+            else if (event.type == sf::Event::MouseButtonPressed) {
+                return;
+            }
+        }
+    }
+}
+
+void findFolder(TreeNode* root, sf::RenderWindow& window, sf::Font& font) {
+    std::string input;
+    sf::Text inputText("", font, 20);
+    inputText.setPosition(50, 50);
+    inputText.setFillColor(sf::Color::White);
+
+    sf::Text prompt("Enter folder name: ", font, 20);
+    prompt.setPosition(50, 20);
+    prompt.setFillColor(sf::Color::White);
+
+    sf::Text outputText("", font, 20);
+    outputText.setPosition(50, 100);
+    outputText.setFillColor(sf::Color::Yellow);
+
+    bool done = false;
+    while (window.isOpen() && !done) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+            else if (event.type == sf::Event::TextEntered) {
+                if (event.text.unicode == '\b' && !input.empty()) {
+                    input.pop_back();
+                }
+                else if (event.text.unicode < 128 && event.text.unicode != '\r' && event.text.unicode != '\n') {
+                    input += static_cast<char>(event.text.unicode);
+                }
+            }
+            else if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Enter) {
+                    done = true;
+                }
+            }
+        }
+
+        inputText.setString(input);
+
+        window.clear();
+        window.draw(prompt);
+        window.draw(inputText);
+        window.display();
+    }
+
+    outputText.setPosition(50, 100);
+    outputText.setFillColor(sf::Color::Yellow);
     
-    cout << "Enter folder name: ";
-    getline(cin, folderName);
-    folderNode = root->findNode(folderName);
-    if (folderNode) {
-        cout << "Number of items in " << folderName << ": " << folderNode->countItems() << endl;
-    }
-    else {
-        cout << "Folder not found." << endl;
-    }
-}
-
-void determineMemoryUsage(TreeNode* root, string folderName, TreeNode* folderNode) {
-    cout << "Enter folder name: ";
-    getline(cin, folderName);
-    folderNode = root->findNode(folderName);
-    if (folderNode) {
-        cout << "Memory used by " << folderName << ": " << folderNode->memoryUsed() << " bytes" << endl;
-    }
-    else {
-        cout << "Folder not found." << endl;
-    }
-}
-
-void pruneEmptyFolders(TreeNode* root) {
-    cout << "Number of empty folders removed: " << root->pruneEmptyFolders() << endl;
-}
-
-void findFolder(TreeNode* root, string folderName) {
-    cout << "Enter the partial or complete filename/folder name to search: ";
-    getline(cin, folderName);
-    string path = root->findPath(folderName);
+    string path = root->findPath(input);
     if (!path.empty()) {
-        cout << "Path: " << path << endl;
+        outputText.setString("Path: " + path);
     }
     else {
-        cout << "File/Folder not found." << endl;
+        outputText.setString("File/Folder not found.");
+    }
+    window.clear();
+    window.draw(prompt);
+    window.draw(inputText);
+    window.draw(outputText);
+    window.display();
+
+    sf::Clock clock;
+    while (window.isOpen() && clock.getElapsedTime().asSeconds() < 4) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+            else if (event.type == sf::Event::MouseButtonPressed) {
+                return;
+            }
+        }
     }
 }
 
-void displayFolderContents(TreeNode* root, string folderName, TreeNode* folderNode) {
-    cout << "Enter folder name to display its contents: ";
-    getline(cin, folderName);
-    folderNode = root->findNode(folderName);
+void displayFolderContents(TreeNode* root, sf::RenderWindow& window, sf::Font& font) {
+    std::string input;
+    sf::Text inputText("", font, 20);
+    inputText.setPosition(50, 50);
+    inputText.setFillColor(sf::Color::White);
+
+    sf::Text prompt("Enter folder name to display contents: ", font, 20);
+    prompt.setPosition(50, 20);
+    prompt.setFillColor(sf::Color::White);
+
+    sf::Text outputText("", font, 15); // Adjust font size for better readability
+    outputText.setPosition(50, 150);    // Adjust position as needed
+    outputText.setFillColor(sf::Color::Yellow);
+
+    bool done = false;
+    while (window.isOpen() && !done) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+            else if (event.type == sf::Event::TextEntered) {
+                if (event.text.unicode == '\b' && !input.empty()) {  // Handle backspace
+                    input.pop_back();
+                }
+                else if (event.text.unicode < 128 && event.text.unicode != '\r' && event.text.unicode != '\n') {
+                    input += static_cast<char>(event.text.unicode);
+                }
+            }
+            else if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Enter) {
+                    done = true;  // User pressed Enter, finish input
+                }
+            }
+        }
+
+        inputText.setString(input);
+
+        // Redraw the window
+        window.clear();
+        window.draw(prompt);
+        window.draw(inputText);
+        window.display();
+    }
+
+    TreeNode* folderNode = root->findNode(input);
     if (folderNode && folderNode->type == "dir") {
-        cout << "Contents of folder \"" << folderName << "\":" << endl;
-        printTree(folderNode, 0, true);  // Print with file size
+        std::stringstream contentStream;
+        printTreeToStream(folderNode, contentStream, 0, true);  // Function to print tree contents to stream
+        outputText.setString(contentStream.str());
     }
     else {
-        cout << "Folder not found or it is a file." << endl;
+        outputText.setString("Folder not found or it is a file.");
+    }
+
+    window.clear();
+    window.draw(prompt);
+    window.draw(inputText);
+    window.draw(outputText);
+    window.display();
+
+    // Wait for a few seconds or until user closes the window
+    sf::Clock clock;
+    while (window.isOpen() && clock.getElapsedTime().asSeconds() < 4) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+            else if (event.type == sf::Event::MouseButtonPressed) {
+                return;
+            }
+        }
     }
 }
 
-void onButtonClick() {
-    std::cout << "Button clicked!" << std::endl;
-    // Add your function logic here
+void printTreeToStream(TreeNode* node, std::stringstream& stream, int depth = 0, bool includeSize = false) {
+    if (!node) return;
+
+    // Indentation for visual hierarchy
+    std::string indentation(depth * 4, ' ');
+
+    // Append node details to stream
+    stream << indentation << "- " << node->name;
+    if (node->type == "file" && includeSize) {
+        stream << " (File, Size: " << node->length << ")";
+    }
+    stream << "\n";
+
+    // Recursive call for children
+    for (auto child : node->children) {
+        printTreeToStream(child, stream, depth + 1, includeSize);
+    }
 }
